5731684495aa8fb97230a41f9f10a2567ca88cdc
ngochuyvu <128366057+NgocHuyVu@users.noreply.github.com>
2023-05-19 16:26:04 +0200

    Add files via upload

--- src/kostka.py
+++ src/kostka.py
@@ -1,12 +1,18 @@
+import random
 class Kostka:
-    def __init__(self, min_hodnota=1, max_hodnota=6):
-        ...
-
-    def hod(self) -> int:
+    def __init__(self):
+        self.kostky = []
+    def hod_kostky(self):
+        kostka_1 = random.randint(1,6)
+        kostka_2 = random.randint(1,6)
+        if kostka_1 == kostka_2:
+            self.kostky = [kostka_1, kostka_1, kostka_2, kostka_2]
+        else:
+            self.kostky = [kostka_1, kostka_2]
         """
         Hod - vygeneruje nahodne cislo od min_hodnota do max_hodnota
 
         Returns:
         int - nahodne cislo od min_hodnota do max_hodnota
         """
-        ...
+        
